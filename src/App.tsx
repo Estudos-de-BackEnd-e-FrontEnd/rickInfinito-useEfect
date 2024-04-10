e495c0d07f1089878cc070f8cab3b21a6a78a719
Geverton Soares <gevertonsoares22@gmail.com>
2024-04-10 20:11:22 -0300

    add style boostrap

--- src/App.tsx
+++ src/App.tsx
@@ -44,34 +44,25 @@ function App() {
         setData(()=> [])
     }
     return (
-        <main >
-            <h1>Rick infinito</h1>
-            <div style={{
-            display: "flex",
-            width: 800,
-            flexWrap: "wrap",
-            margin: "0 auto",
-            gap: "10px"
-        }}>
+        <main className="container-fluid justify-content-center text-center p-0">
+            <div className="container-fluid text-bg-success">
+                <h1 >InfinityRick</h1>
+            </div>
+            
+            <div className="container d-flex justify-content-center flex-wrap" >
                 {data.map((item: Character, index) =>(
-                    <div key={item.id + index }>
-                        <img style={
-                            {
-                                width: "200px"
-                            }
-                        } src={item.image}/>
-                        <h1>{item.name}</h1>
+                    <div className="border rounded border-success m-2 p-2 text-bg-success" key={item.id + index }>
+                        <img className="w-75"  src={item.image}/>
+                        <h1><u>{item.name}</u></h1>
                         <p>{item.species}</p>
                         <p>{item.status}</p>
-                        <button onClick={()=> setCount((prevCount)=> prevCount + 1)}>Clicar</button>
+                        <button className="btn btn-light mb-2" onClick={()=> setCount((prevCount)=> prevCount + 1)}>Clicar</button>
                         <br/>
-                        <button onClick={clear}>Limpar Rick infinito</button>
+                        <button className="btn btn-danger" onClick={clear}>Limpar Rick infinito</button>
                     </div>
 
 
                 ))}
-
-                
             </div>
         </main>
     );
